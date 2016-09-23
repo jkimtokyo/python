11be310679b7b2bf4c935b58de9f338c9b58e9da
jkimtokyo <jkimtokyo@gmail.com>
2016-09-23 11:01:33 +0900

    change directory

--- headfirst.py
+++ headfirst.py
@@ -7,4 +7,20 @@ if guess==5:
     print('You Win!')
 else:
     print('You Lose!')
+print('----------------')
+
+# 분기를 위해서는 들여쓰기
+print('Welcome!')
+g=input('Input input vlaue second chance: ')
+guess=int(g)
+if guess==5:
+    print('You Win!')
+else:
+    if guess>5:
+        print('Sorry, Little bit High')
+    else:
+        print('Sorry, Little bit Low')
+
+# 자 이제 반복. 정답 맞출 때까지... While
+
 
--- while.py
+++ while.py
@@ -1,13 +1,31 @@
 # config=utf-8
-print('   ')
+print('')
 print("Welcome to While Quiz")
-print('---------------')
+print('-----')
 
-guess=0 # 변수 초기화 해 주고...
-while guess != 5: # 어차피 초기 변수가 0이니까..
+from random import randint
+secret = randint(5,7)
+guess = 6 # 변수의 초기화 ??
+
+while guess != secret: # 어차피 초기 변수가 0이니까..
     g=input('Guess the number : ')
     guess = int(g)
-    if guess == 5:
+    if guess == secret:
+        print("Good Answer!")
+    else :
+        print('Try Again....')
+
+print('')
+print("Welcome to While Quiz")
+print('-----')
+
+
+guess = 6 # 변수의 초기화 ??
+
+while guess != 6: # 어차피 초기 변수가 0이니까..
+    g=input('Guess the number : ')
+    guess = int(g)
+    if guess == secret:
         print("Good Answer!")
     else :
         print('Try Again....')
