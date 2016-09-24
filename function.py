00e35b0b0d2ed45052008379cd39569a0ea4d88e
jkimtokyo <jkimtokyo@gmail.com>
2016-09-24 17:20:42 +0900

    타입에러 잘 읽어보자…

--- function.py
+++ function.py
@@ -5,10 +5,12 @@ def getprice():
     page = urllib.request.urlopen('http://beans-r-us.appspot.com/prices-loyalty.html')
     text = page.read().decode('utf-8')
     where = text.find('$')
-    startprice = where+0
+    startprice = where+1
     endprice = where+5
-    print(text[startprice:endprice])
-getprice()
+    return(text[startprice:endprice])
 
-
+price = getprice()
+discount = 0.9
+realprice = float(price) * discount
+print(realprice)
 
