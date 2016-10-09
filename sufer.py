cbf795b724e93064dde5574b5374ce5d66abc67d
jkimtokyo <jkimtokyo@gmail.com>
2016-10-09 14:34:55 +0900

    함수 시작 주일

--- sufer.py
+++ sufer.py
@@ -19,7 +19,7 @@ for file_read in file:
     grut[score] = name
     # dict 괄호 안의 것이 키고 = 뒤의 것이 밸류다. 그런데 복수의 키가 같은 값을 가지면 아래에서 넣은 값이 출력된다. score 순으로 정렬을 해줘야 하기 때문에 키 값으로 점수를 넣는다....
     #print(pair) # pair를 그대로 출력해보자' 포를 썼으니까 하나씩 하나씩 추가되어 나온다;
-print('')
+
 for score_value in grut.keys(): # 키 함수는 딕에서 쓸 수 있는데 키들을 배열로 만든다 즉 키들을 스코어 밸류라는 변수에 넣어라..
     print(score_value, grut[score_value]) # 키에 해당되는 밸류값 가져 오는 것은 a[키] 하면 밸류를 소환한다.
 print('----------')
@@ -34,3 +34,26 @@ for list in sorted(second_grut.keys(), reverse = True): # for 문과 함께 sort
     print(list, second_grut[list])
     open_file.close()
 
+print('----------')
+
+data = '101, john, USA, 8.32, fish, 21'
+grut = {}
+(grut['id'],grut['name'],grut['country'],grut['score'],grut['board'],grut['age']) = data.split(',') # split은 아무 것도 () 안에 넣지 않을 경우 빈칸을 기준으로 한다. 대신 , ; 둘 다 써봐도 문제 없이 작동된다
+# key value dict 만들기 먼저 그릇 = {} 빈 그릇 만들고 a[key] = value 조합임 그런데 여기선 키가 문자열이기 때문에 '' 따옴표로.... 그리고 밸류를 스플릿 해서 하나씩 넣어야 하므로 data.split()
+print(grut)
+print('Country is :',grut['country'])
+
+print('---------')
+
+def finder_function(find_id):
+    file_read = open('surfing_data.csv')
+    for each_line in file_read:
+        grut = {}
+        (grut['id'],grut['name'],grut['country'],grut['score'],grut['type'],grut['age']) = each_line.split(';')
+    if find_id == int(grut['id']):
+        return(grut) # 그릇의 아이디 나이 등의 키가 아닌 1줄을 통째로 리턴한다
+
+input_from = int(input('Type ID Here:'))
+surfer = finder_function(input_from)
+if surfer: # 그냥 이렇게 써도 트루 퍼스 구문이 된다...
+    print(surfer['id'],surfer['name'],surfer['country'],surfer['score'],surfer['type'],surfer['age'])
