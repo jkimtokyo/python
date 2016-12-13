b7a2edc38a458506e7796088df8fac61959fa83c
jkimtokyo <jkimtokyo@gmail.com>
2016-12-13 18:06:18 +0900

    사용자입력과 PRINT출력

---
+++ 사용자입력.py
@@ -0,0 +1,7 @@
+# 사용자 입력을 받기
+# 사용자가 입력한 값을 변수에 대입하고 싶을 때 INPUT, input은 입력되는 모든 자료형을 문자열로 취급해서 받아들인다.
+a = input('input something : ')
+print(a)
+# if user input number 3
+print(type(a)) # <class 'str'> 모든 자료형을 문자열로 취급해서 받아들인다.
+
